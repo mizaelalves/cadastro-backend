8ce39e6960a6c6f97e53f158c0aa9aec6848bf77
Mizael Alves <mizaelbna@hotmail.com>
2021-08-10 23:09:43 -0300

    delete put

--- userRoute.js
+++ userRoute.js
@@ -17,23 +17,6 @@ const userRoute = (app) => {
     await User.create(req.body);
     res.send(req.body);
   });
-  app.put("/users/:id?", async (req, res) => {
-    const { id, name, age, cpf, date } = req.body;
-
-    const userIndex = await User.findByPk((User) => User.id === id);
-
-    if (userIndex < undefined) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    const user = { name, age, cpf, date };
-
-    users[userIndex] = user;
-
-    const save = await user.save();
-    console.log(save);
-    return res.json(user);
-  });
   app.delete("/users/:id?", (req, res) => {
     User.destroy({ where: { id: id } });
 
